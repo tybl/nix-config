038b67ae37261d8b127c3d719019084146693c23
tybl <tybl@oro.lan>
2023-03-24 19:54:25 -0400

    Remove dead code

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
   description = "my minimal flake";
   inputs = {
-    #
-    #
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
 
     home-manager.url = "github:nix-community/home-manager/master";
@@ -25,38 +23,6 @@
             useUserPackages = true;
             users.tybl.imports = [
 	      ./modules/home-manager
-#              ({ pkgs, ... }: {
-#                # Don't change this when you change package input. Leave it alone.
-#                home.stateVersion = "22.11";
-#                # specify home-manager configs
-#                home.packages = [ pkgs.ripgrep pkgs.fd ];
-#                home.sessionVariables = {
-#                  PAGER = "less";
-#                  CLICOLOR = 1;
-#                  EDITOR = "nvim";
-#                };
-#                programs.git = {
-#		  enable = true;
-#		  userName = "tybl";
-#		  userEmail = "tybl@oro.lan";
-#		};
-#                programs.neovim.enable = true;
-#                programs.neovim.defaultEditor = true;
-#		programs.ssh = {
-#		  enable = true;
-#		  extraConfig = ''
-#		    IgnoreUnknown UseKeyChain
-#		    UseKeyChain yes
-#		  '';
-#		};
-#                programs.starship.enable = true;
-#                programs.starship.enableZshIntegration = true;
-#                programs.zsh.enable = true;
-#                programs.zsh.enableCompletion = true;
-#                programs.zsh.enableAutosuggestions = true;
-#                programs.zsh.enableSyntaxHighlighting = true;
-#                programs.zsh.shellAliases = { ls = "ls --color=auto -F"; };
-#              })
             ];
           };
         }
